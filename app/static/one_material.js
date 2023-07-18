0ce6b2f0bebe6234898727877111a5d0ee2577ec
Nikspb2022 <dklsgj@gmail.com>
2023-07-18 22:50:20 +0600

    делаю кнопки в карточке товара

--- app/static/one_material.js
+++ app/static/one_material.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   const params = new URLSearchParams(window.location.search)
   const access_token = params.get("t");
+  let material_id = window.location.href.split("/").pop().split("?")[0];
+  let url = window.location.origin + "/materials/delete?id_for_delete=" + material_id;
   $("#delete").on("click", async function() {
-    let data1 = $("#materials_is").text();
-    let urlParams = new URLSearchParams(window.location.search);
-    let access_token = urlParams.get("t");
-    let url = "http://127.0.0.1:9001/materials/delete?id_for_delete=" + data1;
     try {
       const response = await $.ajax({
         url: url,
@@ -24,3 +22,36 @@ $(document).ready(function() {
     }
   });
 });
+
+
+// событие по клику
+
+$("#relocate").on("click", function () {
+  $("#new-geo-popup").fadeIn(200);
+});
+$("#submit-geo").on("click", async function () {
+    let material_id = window.location.href.split("/").pop().split("?")[0];
+      await update_geo({
+          "material_id": material_id,
+          "place": $('#new-geo-popup input[type="text"]').eq(0).val(),
+          "client_mail": $('#new-geo-popup input[type="text"]').eq(1).val(),
+          "status": $('#new-geo-popup input[type="text"]').eq(2).val(),
+      });
+      try {
+        const response = await $.ajax({
+          url: url,
+          method: "POST",
+          contentType: "application/json",
+          headers: {
+            'Authorization': 'Bearer ' + access_token
+          },
+          body: JSON.stringify(data),
+        });
+        console.log(response);
+        // Обработка успешного удаления товара
+      } catch (error) {
+        console.error(error);
+        // Обработка ошибки удаления товара
+      }
+  });
+  // window.location.reload();
