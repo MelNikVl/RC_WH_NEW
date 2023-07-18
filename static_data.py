c4489656325372efd7423eda0bfbd55b017c544c
Nikspb2022 <dklsgj@gmail.com>
2023-07-18 18:03:51 +0600

    хост

--- static_data.py
+++ static_data.py
@@ -10,7 +10,7 @@ main_folder = os.path.join(current_dir, "app", "it_warehouse_docs")
 bot = Bot("6255903272:AAEYZgt6krIp723UY7mchIN4u-ydVZRLOC0")
 # bot_for_admin = Bot("5858975680:AAFdDI-XOEYqFxNf9OAbx0p6JgpuW89DiqE")
 
-host = "http://localhost:8000"
+host = "http://192.168.1.104:50131"
 
 utilization_ntf_users = ["shumerrr@yandex.ru", "sergey.zagustin@rencons.com", "nikolai.melnik@rencons.com"]
 repair_ntf_users = "shumerrr@yandex.ru, sergey.zagustin@rencons.com"
